24d2e91efbf075d09f6bae12b8bd2e3f4f300922
djjean <djjean90@gmail.com>
2021-10-10 11:20:49 +0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //Script.js
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  // % of vh for element (80%)
+  const triggerBottom = window.innerHeight * 0.8;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
